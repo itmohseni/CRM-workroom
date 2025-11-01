dd2fcdd738cb8149aead99e7f2579259af60a180
itmohseni <mohseni.offical@yahoo.com>
2025-11-01 20:03:27 +0330

    createbase login page

--- CRM-workroom/src/App.jsx
+++ CRM-workroom/src/App.jsx
@@ -1,20 +1,30 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/header";
 import Menu from "./components/menu";
 import Dashboard from "./Dashboard";
+import SignIn from "./sign-in/Sign-in";
 const App = () => {
-  return (
 
-    <div className="flex">
-      <Menu />
-      <div className="w-7/8 py-5 px-[30px]">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-        </Routes>
-      </div>
-    </div>
+  const location = useLocation()
+
+  return (
+    <>
+      {location.pathname === "/sign-in" ? <SignIn /> :
+        <div className="flex">
+          <Menu />
+          <div className="w-7/8 py-5 px-[30px]">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/Dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+        </div>
+
+      }
+
+
+    </>
   );
 }
 
---
+++ CRM-workroom/src/sign-in/Sign-in.jsx
@@ -0,0 +1,18 @@
+
+const SignIn = () => {
+    return (
+        <div className="bg-primary-100 flex">
+            <div className="w-full h-screen flex py-5 px-8">
+                <div className="w-1/2 bg-primary h-full rounded-l-3xl">
+                    <div>
+                        <img src="./src/assets/imags/Companys-logo.png" alt="imag" />
+
+                    </div>
+                </div>
+                <div className="w-1/2 h-full bg-white rounded-r-3xl"></div>
+            </div>
+        </div>
+    );
+}
+
+export default SignIn;
